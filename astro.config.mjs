912a6e7147af0dee788cea671892e3f327240ac0
lachappers <116145040+lachappers@users.noreply.github.com>
2023-08-23 10:27:43 +0100

    added robots.txt and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,15 @@ import { defineConfig } from "astro/config";
 import compress from "astro-compress";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
-
 import partytown from "@astrojs/partytown";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://peculiardynamics.co.uk",
   experimental: {
-    assets: true,
+    assets: true
   },
-  integrations: [compress(), mdx(), tailwind(), partytown()],
+  integrations: [compress(), mdx(), tailwind(), partytown(), sitemap()]
 });
