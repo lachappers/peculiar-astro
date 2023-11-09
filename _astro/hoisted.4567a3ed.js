bb826fabd190669eff9e8fc1e87cb2261d9ac915
lachappers <116145040+lachappers@users.noreply.github.com>
2023-11-09 12:11:57 +0000

    Updates

---
+++ _astro/hoisted.4567a3ed.js
@@ -0,0 +1,1 @@
+import"./hoisted.3ca2b816.js";function n(){const e=document.querySelector(".policy"),t=document.createElement("button");t.setAttribute("data-cc","c-settings"),t.innerHTML="Manage Cookie Settings",t.classList.add("button","color-neutral","size-small","max-w-fit","self-center"),e.parentNode.insertBefore(t,e)}document.getElementById("cookies")&&n();
