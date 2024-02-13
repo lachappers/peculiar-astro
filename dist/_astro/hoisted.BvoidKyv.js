bb9aa0886f3b015dcf4d7977b51ded8f9f1fb06d
lachappers <116145040+lachappers@users.noreply.github.com>
2024-02-13 13:43:29 +0000

    reinstating favicon

--- dist/_astro/hoisted.BvoidKyv.js
+++
@@ -1,1 +0,0 @@
-import"./hoisted.DKHiHzSN.js";const e="aW5mb0BwZWN1bGlhcmR5bmFtaWNzLmNvLnVr",c=document.querySelectorAll(".email-link");c.forEach(t=>{t.setAttribute("href","mailto:".concat(atob(e)).concat("?subject=I've got a Peculiar Query"))});
---
+++ dist/_astro/hoisted.PNcHuI_v.js
@@ -0,0 +1,1 @@
+import"./hoisted.ijPJyojb.js";import"./cookieconsent.esm.DjwhI96l.js";
