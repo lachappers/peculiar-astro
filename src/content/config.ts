5180ec62a7ec8e9470ced31e0e2096bc158a4a63
lachappers <116145040+lachappers@users.noreply.github.com>
2024-02-20 16:52:21 +0000

    removing unused content collections

--- src/content/config.ts
+++ src/content/config.ts
@@ -17,21 +17,10 @@ const blogCollection = defineCollection({
     draft: z.boolean().optional(),
   }),
 });
-// Pages collection schema
-const pagesCollection = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    meta_title: z.string().optional(),
-    description: z.string().optional(),
-    image: z.string().optional(),
-    draft: z.boolean().optional(),
-  }),
-});
 
 // 3. Export a single `collections` object to register your collection(s)
 export const collections = {
   blog: blogCollection,
-  pages: pagesCollection,
 };
 
 // update module - quit console and run npx astro sync
